0c4dc6abbd59231cb6b3f1c3ea726b20dd456d58
Iris <ciciyeung96@gmail.com>
2019-09-29 01:54:48 -0400

    Revised some of the code

--- Transportation_PuLP.py
+++ Transportation_PuLP.py
@@ -8,15 +8,16 @@ from pulp import *
 PLANTS = ["Plant1", "Plant2", "Plant3"]
 #Create a list of regions
 REGIONS = ["Region1", "Region2", "Region3", "Region4"]
+
 #Create a dictionary of unit cost
-shippingCost = { "Plant1" : {"Region1" : 131, "Region2" : 218, "Region3" : 266, "Region4" : 120},
-          		 "Plant2" : {"Region1" : 250, "Region2" : 116, "Region3" : 263, "Region4" : 278},
-          		 "Plant3" : {"Region1" : 178, "Region2" : 132, "Region3" : 122, "Region4" : 188}}
+#                                 REGION 1                     REGION 2                    REGION 3                    REGION 4
+shippingCost = {("Plant1","Region1") : 131, ("Plant1","Region2") : 218, ("Plant1","Region3") : 266, ("Plant1","Region4") : 120,   # PLANT 1
+				("Plant2","Region1") : 250, ("Plant2","Region2") : 116, ("Plant2","Region3") : 263, ("Plant2","Region4") : 278,   # PLANT 2
+          		("Plant3","Region1") : 178, ("Plant3","Region2") : 132, ("Plant3","Region3") : 122, ("Plant3","Region4") : 188}   # PLANT 3
 #Set contraints
 #Create a dictionary of constraints of supply
 supply = {"Plant1": 450,
           "Plant2": 600,
-
           "Plant3": 500}
 #Create a dictionary of constraints of demand
 demand = {"Region1": 450,
@@ -30,27 +31,24 @@ prob = LpProblem("Grand_Prix_Transportation", LpMinimize)
 #Note1: Route pairs are only for transportation problems;
 #Note2: Other problems may use standard variables
 
-#Routes = [(i,j) for i in PLANTS for j in REGIONS]
-Routes = [] #Create an empty list
-for i in PLANTS: #Two dimensional list
-	for j in REGIONS:
-		myTuple = (i, j)
-		Routes.append(myTuple)
+
+# Vars = LpVariable.dicts("Route", (PLANTS,REGIONS), 0, None, LpInteger)
+Vars = {} #Prepare a container to hold the contraint variables
+for i in PLANTS:
+  for j in REGIONS:
+    Vars[i,j] = LpVariable(name='vars('+str(i)+')('+str(j)+')', lowBound = 0, upBound = None, cat = 'Integer')
 
 #Dictionary called "vars" is created to contain referenced variables (routes)
 #These variables are precisely the amount of flow (units shipped) on each route
-Vars = LpVariable.dicts("Route", (PLANTS,REGIONS), 0, None, LpInteger)
 
 
 #The objective function is added to "prob" first
 
-# prob += lpSum([Vars[i][j]*shippingCost[i][j] for (i,j) in Routes]), "Sum_of_Shipping_Costs"
-sumList = [] #Create an empty list 
-for (i,j) in Routes:
-	combinations = Vars[i][j]*shippingCost[i][j]
-	#Use append() function to append new element to a list that was created earlier. For more information visit https://docs.python.org/3/library/array.html?highlight=append#array.array.append
-	sumList.append(combinations) #A list of tuples
-prob += lpSum(sumList), "Sum_of_Shipping_Costs"
+tempList = []
+for i in PLANTS:
+	for j in REGIONS:
+		tempList.append(Vars[i,j]*shippingCost[i,j]) #Number of each shipping * unit shipping cost
+prob += lpSum(tempList), "Sum_of_Shipping_Costs"
 
 
 # Upper "supply" bounds are added to "prob" for each supply node i (plant)
@@ -60,7 +58,7 @@ prob += lpSum(sumList), "Sum_of_Shipping_Costs"
 for i in PLANTS:
 	supplyList = [] #Create a list for each plant containing 4 regions
 	for j in REGIONS:
-		supplyList.append(Vars[i][j])
+		supplyList.append(Vars[i,j])
 	prob += lpSum(supplyList) <= supply[i], "Sum_of_Products_out_of_Plant_%s"%i
 
 
@@ -72,7 +70,7 @@ for i in PLANTS:
 for j in REGIONS:
 	demandList = [] #Create a list for each region cotaining 3 plants
 	for i in PLANTS:
-		demandList.append(Vars[i][j])
+		demandList.append(Vars[i,j])
 	prob += lpSum(demandList) >= demand[j], "Sum_of_Products_into_Region_%s"%j
                    
 
